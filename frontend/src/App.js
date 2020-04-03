25f92f23ccb31abecd3e44333de041f4c5d66766
seojinsarejj <s19009@naver.com>
2020-04-03 20:00:37 +0900

    CREATE main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    memos : []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/memo/');
+      const memos = await res.json();
+      this.setState({
+        memos
+      });
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.memos.map(item => (
+
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <sqan>{item.content}</sqan>
+          </div>
+        ))}
+        </div>
+    );
+  }
+
 }
 
+
 export default App;
+
+
+
